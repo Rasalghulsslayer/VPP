e0dea90f98c6a28ac3709958cddff7031c1d8a3c
lisasko <121612839+lisasko@users.noreply.github.com>
2025-10-21 10:58:53 +0200

    création du main.py
    Ce script exécute le pipeline ETL complet, incluant l'extraction, le nettoyage et le chargement des données.

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Ce script permet de run complétement notre pipeline ETL avec les étapes suivantes :
+1. Extraire les données utilisateurs du fichier CSV
+2. Nettoyer et transformer les données
+3. Charger les données dans la database PostgreSQL 
+
+A run avec: python main.py
+"""
